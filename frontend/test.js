1ce1d3414260956e02ed93358f6eb81c73cbe8ed
David McEwen <david.mcewen@ias.com.co>
2024-04-09 21:23:59 -0500

    a

---
+++ frontend/test.js
@@ -0,0 +1,6 @@
+console.log("HELLO WORLD");
+alert("hello hello compile");
+function getAdmin(bol) {
+    return bol;
+}
+getAdmin(true);
---
+++ frontend/test.ts
@@ -0,0 +1,9 @@
+console.log("HELLO WORLD")
+
+alert("hello hello compile")
+
+function getAdmin(bol: boolean): boolean {
+    return bol;
+}
+
+getAdmin(true)
